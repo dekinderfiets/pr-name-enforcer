20a01f863a82f36d657dcc244327d88bb1b9b6fb
derkinderfietsen <87549954+dekinderfiets@users.noreply.github.com>
2021-07-17 02:14:28 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const getPrNumber = () => {
+  const pullRequest = github.context.payload.pull_request;
+  if (!pullRequest) {
+    return undefined;
+  }
+  return pullRequest.number;
+}
+
+const getPrTitle = async (client) => {
+  const prNumber = getPrNumber();
+  if (!prNumber) {
+    core.error("Could not get pull request number from context, exiting");
+    return;
+  }
+  const { data: pullRequest } = await client.pulls.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: prNumber
+  });
+
+  return pullRequest.title
+}
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+    const regex = new RegExp(core.getInput("regex", { required: true }), core.getInput("regex-flags", { required: false }))
+    const errorMessage = core.getInput("error-message", { required: false }) || `PR name does not match provided regex: ${regex}`
+
+    const client = github.getOctokit(token)
+
+    const prTitle = await getPrTitle(client)
+
+    if (!prTitle.match(regex)) {
+      core.setFailed(errorMessage);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
